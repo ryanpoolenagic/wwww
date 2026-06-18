8571085f1ae2353f2441e5aef953ef931af41ab5
stackmodechris-droid <stackmodechris@gmail.com>
2026-06-18 11:15:02 -0400

    Add Meta Pixel and favicon metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Header } from "@/components/shared/header";
 import { SchemaOrg } from "@/components/shared/schema-org";
 import { WhatsAppButton } from "@/components/shared/whatsapp-button";
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -32,6 +33,14 @@ export const metadata: Metadata = {
   metadataBase: new URL("https://truelegacy.com"),
   alternates: {
     canonical: "/",
+  },
+  icons: {
+    icon: [
+      { url: "/icon.svg", type: "image/svg+xml" },
+      { url: "/logo.jpg", type: "image/jpeg" },
+    ],
+    shortcut: ["/icon.svg"],
+    apple: ["/apple-icon.png"],
   },
   openGraph: {
     title: "Ryan Pool Enagic | Creating A Healthier Future",
@@ -86,6 +95,27 @@ export default function RootLayout({
       className={`${inter.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-background">
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1558712438930519');
+fbq('track', 'PageView');`}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1558712438930519&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <Header />
         <div className="flex-1">{children}</div>
         <Footer />
